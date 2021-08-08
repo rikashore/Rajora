4bdbbb31333f8e1df815019f57849d1a7e20c549
Aarav Navani <aarav.navani@outlook.com>
2021-08-08 19:15:30 +0530

    Added endpoints

---
+++ Rajora/ApiEndpoints.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Rajora.Extensions;
+
+namespace Rajora
+{
+    internal static class ApiEndpoints
+    {
+        internal static class Players
+        {
+            internal static Uri GetPlayer(string tag) => "players/{0}".FormatUri(tag);
+            internal static Uri VerifyPlayerToken(string token) => "players/{0}/verifytoken".FormatUri(token);
+        }
+
+        internal static class Labels
+        {
+            internal static Uri GetPlayerLabels() => "labels/players".FormatUri();
+            internal static Uri GetClanLabels() => "labels/clans".FormatUri();
+        }
+
+        internal static class Goldpass
+        {
+            internal static Uri GetCurrentSeasonGoldpass() => "goldpass/seasons/current".FormatUri();
+        }
+
+        internal static class Locations
+        {
+            internal static Uri GetClanRankingByLocation(string locationId) => "locations/{0}/rankings/clans".FormatUri(locationId);
+            internal static Uri GetClanVersusRankingByLocation(string locationId) => "locations/{0}/rankings/clans-versus".FormatUri(locationId);
+
+            internal static Uri GetPlayerRankingByLocation(string locationId) => "locations/{0}/rankings/players".FormatUri(locationId);
+            internal static Uri GetPlayerVersusRankingByLocation(string locationId) => "locations/{0}/rankings/players-versus".FormatUri(locationId);
+
+            internal static Uri GetLocations() => "locations".FormatUri();
+            internal static Uri GetLocation(string locationId) => "locations/{0}".FormatUri(locationId);
+        }
+
+        internal static class Leagues
+        {
+            internal static Uri GetLeagues() => "leagues".FormatUri();
+            internal static Uri GetLeague(string leagueId) => "leagues/{0}".FormatUri(leagueId);
+
+            internal static Uri GetLeagueBySeasonId(string leagueId, string seasonId) => "leagues/{0}/seasons/{1}".FormatUri(leagueId, seasonId);
+            internal static Uri GetLeagueSeasons(string leagueId) => "leagues/{0}/seasons".FormatUri(leagueId);
+
+            internal static Uri GetWarleagues() => "warleagues".FormatUri();
+            internal static Uri GetWarleague(string leagueId) => "warleagues/{0}".FormatUri(leagueId);
+        }
+
+        internal static class Clans
+        {
+            internal static Uri GetClanCurrentWarLeagueGroup(string clanTag) => "clans/{0}/currentwar/leaguegroup".FormatUri(clanTag);
+            internal static Uri GetClanLeagueWar(string warTag) => "clanwarleagues/wars/{0}".FormatUri(warTag);
+            internal static Uri GetClanCurrentWar(string clanTag) => "clans/{0}/currentwar".FormatUri(clanTag);
+
+            internal static Uri GetClanWarlog(string clanTag) => "clans/{0}/warlog".FormatUri(clanTag);
+
+            internal static Uri SearchClans() => "clans".FormatUri();
+            internal static Uri GetClan(string clanTag) => "clans/{0}".FormatUri(clanTag);
+
+            internal static Uri GetClanMembers(string clanTag) => "clans/{0}/members".FormatUri(clanTag);
+        }
+    }
+}
---
+++ Rajora/Extensions/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rajora.Extensions
+{
+    internal static class Extensions
+    {
+        internal static Uri FormatUri(this string s, params object[] args)
+            => new Uri(new Uri("https://api.clashofclans.com/v1"), new Uri(string.Format(s, args), UriKind.Relative));
+
+        internal static Uri AddQueryParams(this Uri uri, IDictionary<string, object> queries)
+        {
+            var builder = new UriBuilder(uri);
+
+            foreach (var (key, value) in queries)
+                builder.Query += $"{key}={value}";
+
+            return builder.Uri;
+        }
+    }
+}

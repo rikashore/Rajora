187330f2128229e292c3f3e0c49980f15e2f18b1
Aarav Navani <aarav.navani@outlook.com>
2021-08-09 15:16:59 +0530

    Updated models

--- Rajora/Models/ClanWarlog/ClanWarlogEntry.cs
+++ Rajora/Models/ClanWarlog/ClanWarlogEntry.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 
 namespace Rajora
 {
-    public class ClanWarlogEntry : BaseJsonModel
+    public class ClanWarlogEntry
     {
         [JsonProperty("clan")]
         public WarlogClan Clan { get; set; }
--- Rajora/Models/GoldPass/CurrentSeasonGoldpass.cs
+++ Rajora/Models/GoldPass/CurrentSeasonGoldpass.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json;
 
-namespace Rajora.GoldPass
+namespace Rajora
 {
     public class CurrentSeasonGoldpass : BaseJsonModel
     {

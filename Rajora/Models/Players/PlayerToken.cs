8a6167b886261826b3eba697d1698a9551200e79
Aarav Navani <aarav.navani@outlook.com>
2021-08-09 15:17:15 +0530

    Added player token model

---
+++ Rajora/Models/Players/PlayerToken.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Rajora
+{
+    public class PlayerToken
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+
+        public PlayerToken(string token)
+        {
+            Token = token;
+        }
+    }
+}

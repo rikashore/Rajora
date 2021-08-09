a6bee086ca3d9d03d50fccde9e472db85ba98386
Aarav Navani <aarav.navani@outlook.com>
2021-08-09 15:17:07 +0530

    Added new error model

---
+++ Rajora/Models/Error.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Rajora
+{
+    public class Error
+    {
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("object")]
+        public JObject Object { get; set; }
+    }
+}

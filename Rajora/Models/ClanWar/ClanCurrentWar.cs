19209cf1ecb59fb5e02f4f93c326cdf880b116fb
Aarav Navani <aarav.navani@outlook.com>
2021-08-09 15:16:43 +0530

    Added current war model

---
+++ Rajora/Models/ClanWar/ClanCurrentWar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Rajora
+{
+    public class ClanCurrentWar
+    {
+        [JsonProperty("clan")]
+        public WarlogClan Clan { get; set; }
+
+        [JsonProperty("teamSize")]
+        public int TeamSize { get; set; }
+
+        [JsonProperty("opponent")]
+        public WarlogClan Opponent { get; set; }
+
+        [JsonProperty("endTime")]
+        public DateTimeOffset? EndTime { get; set; }
+
+        [JsonProperty("startTime")]
+        public DateTimeOffset StartTime { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("preparationStartTime")]
+        public DateTimeOffset PreparationStartTime { get; set; }
+    }
+}
